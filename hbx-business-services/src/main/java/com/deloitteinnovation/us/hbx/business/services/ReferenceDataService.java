b1538c4280e4ea79cf0f20be61bc37b6d744365c
nihalrajjavali <nihalrajjavali@gmail.com>
2023-06-05 16:14:48 +0530

    delete

--- hbx-business-services/src/main/java/com/deloitteinnovation/us/hbx/business/services/ReferenceDataService.java
+++
@@ -1,7 +0,0 @@
-package com.deloitteinnovation.us.hbx.business.services;
-
-import com.deloitteinnovation.us.hbx.entity.referencedata.ReferenceTable;
-
-public interface ReferenceDataService {
-    ReferenceTable getReferenceDataInCacheByType(String refType);
-}
--- hbx-data-services/src/main/java/com/deloitteinnovation/us/hbx/data/services/ReferenceDataDao.java
+++
@@ -1,7 +0,0 @@
-package com.deloitteinnovation.us.hbx.data.services;
-
-import com.deloitteinnovation.us.hbx.entity.referencedata.ReferenceTable;
-
-public interface ReferenceDataDao {
-    ReferenceTable getReferenceDataInCacheByType(String refType);
-}
